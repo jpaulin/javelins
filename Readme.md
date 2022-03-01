379101a18d2ff513dfd0c1bf827436a684864516
Jukka Paulin <>
2022-03-01 12:17:54 +0200

    first

---
+++ Readme.md
@@ -0,0 +1,16 @@
+# javelins
+
+A simple 'blog' if you will. 
+
+Runs when you have NodeJS installed. 
+
+This server will create a listener on port 3000 (you can choose the port)
+
+## Changing port
+
+## Running the server
+
+node ./index.js
+
+Soon a text should appear on your console:
+'Running!'
---
+++ index.js
@@ -0,0 +1,48 @@
+// Six to seven lines server (backend).
+// README:
+// First install Node, and now you have
+// 'npm' tool. Use: 'npm install express' in your working folder. Now you have express.
+const express = require('express') // let's import
+// We make one instance of the Express server, by calling it like this:
+const app = express()
+const port = 3000
+
+function getCompletePostHtml (blogPostId) {
+  let cur = ''
+  cur = '<HTML><BODY>'
+  let postText = posts[blogPostId]
+  cur += postText
+  cur += '</BODY></HTML>'
+  return cur
+}
+
+// This is our blog's posts, so far. They could be complex, but this is a simple example.
+// If you had more, just make them strings like this. Strings can be long in JavaScript!
+const posts = [
+  'First post contains this line!',
+  'Second post',
+  'Third post!',
+  'Greetings is the fourth post!',
+  'Fifth post!']
+
+app.get('/', (req, res) => {
+  res.send('SIXLINE BLOG. I am all good!')
+})
+
+// First and only route. We respond at http://server/post/?id=1
+app.get('/post/', (req, res) => {
+  let postId = parseInt(req.query.id)
+  if (typeof postId !== 'number') {
+    res.send('Internal server query parsing problem. Try again!')
+    return
+  }
+  // Array out of bounds test, for not crashing our server
+  console.log(`Demanded post: ${postId} and my blog has ${posts.length} posts. `)
+  if (postId <= posts.length) {
+    res.send(getCompletePostHtml(postId))
+  } else {
+    res.send("No such post!")
+  }
+})
+
+app.listen(port, () => console.log(`Javelins server listening at http://localhost:${port}`))
